575e86b2fc8bbe1fbd5d24ae14842c5531063a2f
PeiHsaun.Huang <pei@46.207.eduroam.dynamic.rbg.tum.de>
2017-06-02 18:22:33 +0200

    the 1st version of landing page

--- src/components/LandingPage/index.tsx
+++ src/components/LandingPage/index.tsx
@@ -7,7 +7,15 @@ export interface landingPageProps {
 
 class LandingPage extends React.Component<landingPageProps, undefined> {
   render() {
-    return <div>Landing Page! <Link to="/example" className={styles.warnBg}> Link to Example</Link></div>;
+    return<body className={styles.container}>
+    <header role="banner" className={styles.banner}><scan className={styles.pre}>pre</scan>CICE - LandingPage</header>
+    <div className={styles.small}>
+      <div className={styles.intro}>Coupling tool for partitioned simulations<br></br>of multi-physics scenarios.</div>
+      <div className={styles.sub_intro}>Make Coupling Easy Again</div>
+      <div className={styles.Btn_container}><Link to="/example" className={styles.Btn}> Start Now</Link></div>
+    </div>
+    <footer className={styles.footer}>Copyright &copy; <scan className={styles.pre}>pre</scan>CICE</footer>
+    </body>;
   }
 }
 
