6cb72d772542aef4ba7464b17c2771343e3f1a77
hasanash <hasanash112@gmail.com>
2017-11-03 01:55:48 +0100

    deleted dist

--- dist/containers/Router/selectors.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.locationSelector = (state) => {
-    return state.getIn(['route']).location;
-};
-//# sourceMappingURL=selectors.js.map
