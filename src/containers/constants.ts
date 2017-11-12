81e44ce03fa04bc89f0357e496fad6939c8bf32a
hasanash <hasanash112@gmail.com>
2017-11-12 22:00:47 +0100

    dummy

--- src/containers/constants.ts
+++ src/containers/constants.ts
@@ -4,20 +4,13 @@ export const HID_CHECK2 = 'actions/HID_CHECK2';
 export const HID_CHECK3 = 'actions/HID_CHECK3';
 export const XML_CLICK = 'actions/XML_CLICK';
 export const XML_ALL_CLICK = 'actions/XML_ALL_CLICK';
-
-
 export const MODAL_CLICK = 'actions/MODAL_CLICK';
 export const FIRST_TASK_COMPLETED = 'actions/FIRST_TASK_COMPLETED';
 export const IVE_READ = 'actions/IVE_READ';
 export const INITIAL_RELAXATION_CHANGE = 'actions/INITIAL_RELAXATION_CHANGE';
-
 export const xmlBackgroundColor = '#505050';
 export const xmlEmphasizeBackgroundColor = '#8f4746';
-
-
 export const ADD_CHART_DATA = 'actions/ADD_CHART_DATA';
-
-
 export const ADD_PROGRESS_MAX_ITER = 'actions/ADD_PROGRESS_MAX_ITER';
 export const CONSOLE_ONE_ACTIVE = 'actions/CONSOLE_ONE_ACTIVE';
 export const CONSOLE_TWO_ACTIVE = 'actions/CONSOLE_TWO_ACTIVE';
