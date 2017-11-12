0b78dc955ec4d4338f8a854e1d1ac613a1035706
hasanash <hasanash112@gmail.com>
2017-11-12 22:09:42 +0100

    threw away dist

--- dist/containers/Example/selectors.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const reselect_1 = require("reselect");
-const exampleSelector = (state) => {
-    return state.get('example');
-};
-exports.exampleFieldSelector = () => reselect_1.createSelector(exampleSelector, (exampleSubstate) => exampleSubstate.get('exampleField'));
-//# sourceMappingURL=selectors.js.map
