7a286d3ddf6b8025ecc2c7b0306c5895f557bac8
Pei-Hsuan Huang <peihsuan.huang@tum.de>
2018-03-09 18:26:19 +0100

    links

--- src/containers/Final/index.tsx
+++ src/containers/Final/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as styles from './styles.scss';
+import * as github from '../../static/github.png';
 
 interface FinalProps {
 }
@@ -13,10 +14,16 @@ class Final extends React.Component<FinalProps, any> {
           <div className={styles.subIntro}>
             You have finished the tutorial.<br/>
             If you want to run it on your computer, <br/>
-            please go to the github repository <br/>
+            please go to the<span> </span>
+            <a
+              target="_blank"
+              href="https://github.com/precice/precice/wiki/FSI-tutorial"
+              className={styles.githubLink}
+            >github<img src={github} className={styles.img}/>
+            </a>
+            <span> </span>repository <br/>
             and follow the instructions.
           </div>
-          <div className={styles.btnContainer}><a target="_blank" href="https://github.com/precice/precice/wiki/FSI-tutorial" className={styles.btn}> preCICE github</a></div>
           <div className={styles.feedbackText}>We are curious about your feedback on this tutorial.<br />
             Please help us further improve the user experience by<span> </span>
             <a
