6a44bbdccbc411b2ae47564db9c60e8618ba9b30
shkodm <dmytro.sashko@tum.de>
2018-06-04 10:59:16 +0200

    Added menu bar popup and progress bar

--- src/components/Root/index.tsx
+++ src/components/Root/index.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import { Link } from 'react-router-dom';
 import * as styles from './styles.scss';
 import * as preiceLogo from '../../static/Precice-logo.png';
+import * as preciceLogoCropped from '../../static/Precice-logo-cropped.png' 
+import Popup from "reactjs-popup"
 
 interface RootProps {
   children: any;
@@ -14,7 +16,27 @@ class Root extends React.Component<RootProps, undefined> {
         <div className={styles.banner}>
         {/* logo */} 
         <div className={styles.label}> 
-           <img src={preiceLogo} className={styles.img}/>
+
+        <Popup trigger={        <div className={styles.menuButton}> 
+            <i className="fa fa-bars" aria-hidden="true"/>  
+            </div> } 
+        			position="bottom left" 
+        			on="click"
+        			arrow={false}
+        			contentStyle={{ padding: "0px", borderRadius: "15px"}}
+                    closeOnDocumentClick={true} 
+        			> 
+        		<div className={styles.menu}> 
+        		<div className={styles.menuItem}> <Link to="/tutorial/part1/step1" > Part 1. Introduction </Link> </div>
+        		<div className={styles.menuItem}> <Link to="/tutorial/part2/step1" > Part 2. Longer simulation  </Link> </div> 
+        		<div className={styles.menuItem}> <Link to="/tutorial/part3/step1" > Part 3: Implicit coupling </Link> </div> 
+        		<div className={styles.menuItem}> <Link to="/tutorial/part4/step1" > Part 4: Quasi-Newton post-processing  </Link> </div> 
+        		<div className={styles.menuItem}> <Link to="/tutorial/part5/step1" > Part 5: Parallel Coupling </Link> </div> 
+        		</div>
+        		</Popup>
+        <Link to="/" className={styles.label}>{/*preCICE label*/}
+        <img src={preciceLogoCropped} className={styles.img}/> 
+        </Link> 
         </div> 
             {/* </Link>{/*preCICE label*/} 
         </div> 
