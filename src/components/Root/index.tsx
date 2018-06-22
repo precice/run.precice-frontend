e137e5ea60a52053d49af6c46b554f44b04fc8b8
shkodm <dmytro.sashko@tum.de>
2018-06-22 12:50:16 +0200

    Styling of the menubar
    - Padding between different sections
    - Border color on the button
    - Font and name changes
    - Second row of media button
    - Custom scrollbar

--- src/components/Root/index.tsx
+++ src/components/Root/index.tsx
@@ -8,6 +8,7 @@ import * as rglogo from '../../static/RG_square_green.png'
 import Popup from "reactjs-popup"
 import {slide as Menu} from 'react-burger-menu'
 import ExpandableList from "../Expandable"
+import ScrollArea from 'react-scrollbar' 
 
 const burgerStyles = {
   bmBurgerButton: {
@@ -15,7 +16,9 @@ const burgerStyles = {
     width: '26px',
     height: '20px',
     left: '20px',
-    top: '20px'
+    top: '20px',
+	background: '#dddddd',
+	border: '5px solid #dddddd'
   },
   bmBurgerBars: {
     background: '#373a47'
@@ -29,7 +32,7 @@ const burgerStyles = {
   },
   bmMenu: {
     background: '#dddddd',
-    padding: '1.5em 0.5em 0',
+    padding: '1.5em 1.0em 0',
     fontSize: '1.15em',
     overflow: 'hidden'
   },
@@ -64,16 +67,28 @@ class Root extends React.Component<RootProps, undefined> {
             <img src={preiceLogo} className={styles.img}/>
           </div>
       <div className={styles.menu}>
-      <ExpandableList header="Part 1. Introduction" partNumber={1}/>
-      <ExpandableList header="Part 2. Longer simulation"  partNumber={2}/>
-      <ExpandableList header="Part 3: Implicit coupling" partNumber={3}/>
-      <ExpandableList header="Part 4: Quasi-Newton post-processing"  partNumber={4}/>
-      <ExpandableList header="Part 5: Parallel Coupling" partNumber={5} />
+	 <ScrollArea 
+		horizontal={false}
+	  speed={0.6}
+	  verticalScrollBarStyle={{opacity: '0.5 !important'}}
+	>
+		<ExpandableList header="Introduction" partNumber={1}/>
+		<ExpandableList header="Longer simulation"  partNumber={2}/>
+		<ExpandableList header="Implicit coupling" partNumber={3}/>
+		<ExpandableList header="Quasi-Newton post-processing"  partNumber={4}/>
+		<ExpandableList header="Parallel Coupling" partNumber={5} />
+	  </ScrollArea>
       </div>
       <div>
+	{/* Social icons */} 
       <div className={styles.mediaBar}>
-        <div className={styles.mediaItem}> <a href="https://twitter.com/preCICE_org" target="_blank"> <i className="fa fa-twitter" style={{color: '#00aced'}}></i> </a> </div>
-        <div className={styles.mediaItem}> <a href="https://github.com/precice/precice" target="_blank" style={{color: "black"}}> <i className="fa fa-github"></i> </a> </div>
+        <div className={styles.mediaItem}> <a href="https://mailman.informatik.uni-stuttgart.de/mailman/listinfo/precice" target="_blank" style={{color: "black"}}> <i className="fa fa-envelope"></i> </a> </div>
+        <div className={styles.mediaItem}> <a href="https://gitter.im/precice/Lobby" target="_blank"> <div className={styles.gitterSvg}> </div> </a> </div>
+        <div className={styles.mediaItem}> <a href="https://twitter.com/preCICE_org" target="_blank"> <i className="fa fa-twitter" style={{color: '#00aced', width: '35px'}}></i> </a> </div>
+      </div>
+	{/* Research icons */} 
+      <div className={styles.mediaBar}>
+        <div className={styles.mediaItem}> <a href="https://github.com/precice/precice" target="_blank" style={{color: "black"}}> <i className="fa fa-github" style={{width: '35px'}}></i> </a> </div>
         <div className={styles.mediaItem}> <a href="https://www.researchgate.net/project/preCICE" target="_blank"> <img src={rglogo} /> </a> </div>
         <div className={styles.mediaItem}> <a href="https://www.precice.org/" target="_blank"> <img src={preciceCircle} /> </a> </div>
       </div>
