e14be05e04bfe257b92517c2c528049cf4afee89
Gerasimos Chourdakis <chourdak@in.tum.de>
2019-02-03 09:01:10 +0100

    IQN: Add note about performance

--- src/containers/Step2WhatToDo/Part4WhatToDo/index.tsx
+++ src/containers/Step2WhatToDo/Part4WhatToDo/index.tsx
@@ -10,7 +10,7 @@ class Part4Sub extends React.Component<Part4SubProps, any> {
       <div className={styles.textStyle}>
         The xml file below has already been changed to use the Inverse Least-Squares method, IQN-ILS in preCICE,
         as the post-processing step. This method reuses information from previous time steps, which can increase the
-        speed of convergence. Details can be found<span> </span>
+        speed of convergence. However, the performance benefit cannot be demonstrated in this small-scale example. Details can be found<span> </span>
         <a className={styles.link} target="_blank" href={'https://github.com/precice/precice/wiki/Quasi-Newton-Post-Processing-Methods'}>here</a>.
       </div>
     );
